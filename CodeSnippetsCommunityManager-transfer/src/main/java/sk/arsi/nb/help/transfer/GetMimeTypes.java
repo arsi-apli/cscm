e6a2da08c5e0aff9d61e168f44da792ff6fc2cf9
Arsi <arsi-apli@users.noreply.github.com>
2017-08-06 16:27:31 +0200

    Classes for Snippets browser

---
+++ CodeSnippetsCommunityManager-transfer/src/main/java/sk/arsi/nb/help/transfer/GetMimeTypes.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package sk.arsi.nb.help.transfer;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author arsi
+ */
+public class GetMimeTypes implements Serializable {
+
+}
