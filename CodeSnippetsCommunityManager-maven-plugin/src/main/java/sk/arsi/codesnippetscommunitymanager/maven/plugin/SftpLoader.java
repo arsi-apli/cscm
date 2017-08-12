4ec9bc8e732580a373b334c614ab8f29b412d2b7
Arsi <arsi-apli@users.noreply.github.com>
2017-08-12 20:27:41 +0200

    maven plugin to upload netbeans autoupdate site

---
+++ CodeSnippetsCommunityManager-maven-plugin/src/main/java/sk/arsi/codesnippetscommunitymanager/maven/plugin/SftpLoader.java
@@ -0,0 +1,112 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sk.arsi.codesnippetscommunitymanager.maven.plugin;
+
+import com.jcraft.jsch.*;
+import java.io.*;
+import java.util.StringTokenizer;
+
+/**
+ * Created by krishna on 29/03/2016.
+ */
+public class SftpLoader {
+
+    ChannelSftp channel;
+    final String host;
+    final String userName;
+    final String password;
+    private Session session;
+
+    public SftpLoader(String host, String userName, String password) {
+        this.host = host;
+        this.userName = userName;
+        this.password = password;
+    }
+
+    public void connect() throws JSchException {
+
+        JSch jsch = new JSch();
+        session = jsch.getSession(userName, host);
+        session.setPassword(password);
+        java.util.Properties config = new java.util.Properties();
+        config.put("StrictHostKeyChecking", "no");
+        session.setConfig(config);
+        session.connect();
+        channel = (ChannelSftp) session.openChannel("sftp");
+        channel.connect();
+    }
+
+    public void disconnect() {
+        channel.disconnect();
+        session.disconnect();
+    }
+
+    private void cdOrCreateDirs(String remoteDir) throws SftpException {
+        StringTokenizer tok = new StringTokenizer(remoteDir, "/", false);
+        while (tok.hasMoreElements()) {
+            String nextdir = tok.nextToken();
+            try {
+                channel.stat(nextdir);
+            } catch (Exception e) {
+                channel.mkdir(nextdir);
+            }
+            channel.cd(nextdir);
+        }
+    }
+
+    public void transferDirToRemote(String localDir, String remoteDir) throws SftpException, FileNotFoundException {
+        channel.cd("/");
+        File localFile = new File(localDir);
+        cdOrCreateDirs(remoteDir);
+
+        // for each file  in local dir
+        for (File localChildFile : localFile.listFiles()) {
+
+            // if file is not dir copy file
+            if (localChildFile.isFile()) {
+                transferFileToRemote(localChildFile.getAbsolutePath(), remoteDir);
+
+            } // if file is dir
+            else if (localChildFile.isDirectory()) {
+
+                // mkdir  the remote
+                SftpATTRS attrs;
+                try {
+                    attrs = channel.stat(localChildFile.getName());
+                } catch (Exception e) {
+                    channel.mkdir(localChildFile.getName());
+                }
+
+                // repeat (recursive)
+                transferDirToRemote(localChildFile.getAbsolutePath(), remoteDir + "/" + localChildFile.getName());
+                channel.cd("..");
+            }
+        }
+
+    }
+
+    public void transferFileToRemote(String localFile, String remoteDir) throws SftpException, FileNotFoundException {
+        channel.cd(remoteDir);
+        channel.put(new FileInputStream(new File(localFile)), new File(localFile).getName(), ChannelSftp.OVERWRITE);
+    }
+
+    public void transferToLocal(String remoteDir, String remoteFile, String localDir) throws SftpException, IOException {
+        channel.cd(remoteDir);
+        byte[] buffer = new byte[1024];
+        BufferedInputStream bis = new BufferedInputStream(channel.get(remoteFile));
+
+        File newFile = new File(localDir);
+        OutputStream os = new FileOutputStream(newFile);
+        BufferedOutputStream bos = new BufferedOutputStream(os);
+
+        int readCount;
+        while ((readCount = bis.read(buffer)) > 0) {
+            bos.write(buffer, 0, readCount);
+        }
+        bis.close();
+        bos.close();
+    }
+}
---
+++ CodeSnippetsCommunityManager-maven-plugin/src/main/java/sk/arsi/codesnippetscommunitymanager/maven/plugin/UpdateSiteMojo.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sk.arsi.codesnippetscommunitymanager.maven.plugin;
+
+import java.io.File;
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.plugins.annotations.ResolutionScope;
+import org.apache.maven.project.MavenProject;
+import org.apache.maven.settings.Server;
+import org.apache.maven.settings.Settings;
+
+/**
+ *
+ * @author arsi
+ */
+@Mojo(name = "updatesite",
+        defaultPhase = LifecyclePhase.DEPLOY,
+        aggregator = true,
+        requiresDependencyResolution = ResolutionScope.RUNTIME, threadSafe = true)
+public class UpdateSiteMojo extends AbstractMojo {
+
+    @Parameter(required = true, readonly = true, property = "project")
+    private MavenProject project;
+
+    @Parameter(required = true, readonly = true, property = "serverId")
+    private String serverId;
+
+    @Parameter(required = true, readonly = true, property = "sftpUrl")
+    private String sftpUrl;
+
+    @Parameter(required = true, readonly = true, property = "sftpPath")
+    private String sftpPath;
+
+    @Parameter(required = true, readonly = true, property = "netbeansSiteDir")
+    private File netbeansSiteDir;
+
+
+    @Parameter(defaultValue = "${settings}", readonly = true)
+    private Settings settings;
+    private Server server;
+
+    @Override
+    public void execute() throws MojoExecutionException, MojoFailureException {
+        server = settings.getServer(serverId);
+        if (server == null) {
+            throw new MojoExecutionException("No server defined!");
+        }
+        SftpLoader loader = new SftpLoader(sftpUrl, server.getUsername(), server.getPassword());
+        try {
+            loader.connect();
+            loader.transferDirToRemote(netbeansSiteDir.getCanonicalPath(), sftpPath);
+            loader.disconnect();
+        } catch (Exception ex) {
+            throw new MojoExecutionException(ex.getMessage());
+        }
+    }
+
+}
