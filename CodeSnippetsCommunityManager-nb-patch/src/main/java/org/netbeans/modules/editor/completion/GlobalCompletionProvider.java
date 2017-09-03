5651a42b623fefd7c1f9392294f7dfdbc68fa91c
Arsi <arsi-apli@users.noreply.github.com>
2017-09-03 09:16:44 +0200

    rework - NETBEANS81

---
+++ CodeSnippetsCommunityManager-nb-patch/src/main/java/org/netbeans/modules/editor/completion/GlobalCompletionProvider.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.netbeans.modules.editor.completion;
+
+import org.netbeans.spi.editor.completion.CompletionProvider;
+
+/**
+ *
+ * @author arsi
+ */
+public interface GlobalCompletionProvider extends CompletionProvider {
+
+}
